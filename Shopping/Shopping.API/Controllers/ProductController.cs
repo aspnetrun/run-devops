652e4667e9fc429df6e93d0faed974df8c9df3cf
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-01-15 17:51:09 +0300

    ProductController

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "asd"
+            })
+            .ToArray();
+        }
+
+    }
+}
