50da80edeaed0a2c179b63af71b9472a7a987441
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-01-23 20:52:50 +0300

    api last pipeline

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -76,7 +76,7 @@ namespace Shopping.API.Data
                 },
                 new Product()
                 {
-                    Name = "LG G7 ThinQ New10",
+                    Name = "LG G7 ThinQ EndofCourse",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-6.png",
                     Price = 240.00M,
