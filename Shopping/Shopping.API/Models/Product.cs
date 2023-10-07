57f694a57575ee42a2b3b19843a0baaa0e91964a
Chaitanya Korupolu <chaitu51402@gmail.com>
2023-10-07 22:32:22 +0530

    Lesson 23 ending

---
+++ Shopping/Shopping.API/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+
+
+    }
+}
